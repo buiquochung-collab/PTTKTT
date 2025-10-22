58bd1516a5b51dea0201bc8a6a5e9d36e9184035
buiquochung-collab <buiquochung0942@gmail.com>
2025-10-23 00:27:03 +0700

    Update PrimController.cs

--- Controllers/PrimController.cs
+++ Controllers/PrimController.cs
@@ -1,17 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using Prim_Kruskal_Web.Models;
+using System;
 using System.Web.Mvc;
 
 namespace Prim_Kruskal_Web.Controllers
 {
     public class PrimController : Controller
     {
-        // GET: Prim: gọi giải thuật prim từ service, trả về kết quả
+        DatabaseContext db = new DatabaseContext();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult RunPrim()
+        {
+            try
+            {
+                var graph = db.LoadGraph();
+                if (graph == null || graph.Nodes.Count == 0)
+                    return Content("Không có dữ liệu trong đồ thị.");
+
+                var mstEdges = Prim.FindMST(graph); // phần này cần có class Prim.cs
+                var result = new MSTResult(graph, mstEdges);
+
+                // Trả về view mô phỏng chung
+                return View("~/Views/Home/MoPhong.cshtml", result);
+            }
+            catch (Exception ex)
+            {
+                return Content("Lỗi khi chạy Prim: " + ex.Message);
+            }
         }
     }
 }
